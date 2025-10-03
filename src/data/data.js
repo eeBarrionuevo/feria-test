6285abd1e9e94c5826b38082d6462f12da940c78
Elvis Barrionuevo <idcareg.g@gmail.com>
2025-10-03 10:22:31 -0500

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,314 @@
+export const logos = [
+  {
+    name: "Amazon",
+    src: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+  },
+  {
+    name: "Microsoft",
+    src: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+  },
+  {
+    name: "Google",
+    src: "https://upload.wikimedia.org/wikipedia/commons/5/51/Google.png",
+  },
+  {
+    name: "Netflix",
+    src: "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+  },
+  {
+    name: "Apple",
+    src: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    company: "Ferreyros",
+    image:
+      "https://utp.hiringroomcampus.com/assets/media/utp/company_5ebb0d0608a25a527a281371.png",
+    opportunities: 4,
+  },
+  {
+    id: 2,
+    company: "Aceros Arequipa",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/05/Aceros_Logo.png",
+    opportunities: 7,
+  },
+  {
+    id: 3,
+    company: "Manpower",
+    image:
+      "https://www.manpowergroup.pe/wps/wcm/connect/manpowergroup/33fe94d4-3ca4-48ee-8842-6c1166ee515b/200x200-logo-manpower.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_2802IK01OORA70QUFIPQ192H31-33fe94d4-3ca4-48ee-8842-6c1166ee515b-mbU.Nf2",
+    opportunities: 5,
+  },
+  {
+    id: 4,
+    company: "Ferreyros",
+    image:
+      "https://utp.hiringroomcampus.com/assets/media/utp/company_5ebb0d0608a25a527a281371.png",
+    opportunities: 4,
+  },
+  {
+    id: 5,
+    company: "Aceros Arequipa",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/05/Aceros_Logo.png",
+    opportunities: 7,
+  },
+  {
+    id: 6,
+    company: "Manpower",
+    image:
+      "https://www.manpowergroup.pe/wps/wcm/connect/manpowergroup/33fe94d4-3ca4-48ee-8842-6c1166ee515b/200x200-logo-manpower.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_2802IK01OORA70QUFIPQ192H31-33fe94d4-3ca4-48ee-8842-6c1166ee515b-mbU.Nf2",
+    opportunities: 5,
+  },
+  {
+    id: 7,
+    company: "Ferreyros",
+    image:
+      "https://utp.hiringroomcampus.com/assets/media/utp/company_5ebb0d0608a25a527a281371.png",
+    opportunities: 4,
+  },
+  {
+    id: 8,
+    company: "Aceros Arequipa",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/05/Aceros_Logo.png",
+    opportunities: 7,
+  },
+  {
+    id: 9,
+    company: "Manpower",
+    image:
+      "https://www.manpowergroup.pe/wps/wcm/connect/manpowergroup/33fe94d4-3ca4-48ee-8842-6c1166ee515b/200x200-logo-manpower.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_2802IK01OORA70QUFIPQ192H31-33fe94d4-3ca4-48ee-8842-6c1166ee515b-mbU.Nf2",
+    opportunities: 5,
+  },
+    {
+    id: 10,
+    company: "Ferreyros",
+    image:
+      "https://utp.hiringroomcampus.com/assets/media/utp/company_5ebb0d0608a25a527a281371.png",
+    opportunities: 4,
+  },
+  {
+    id: 11,
+    company: "Aceros Arequipa",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/05/Aceros_Logo.png",
+    opportunities: 7,
+  },
+  {
+    id: 12,
+    company: "Manpower",
+    image:
+      "https://www.manpowergroup.pe/wps/wcm/connect/manpowergroup/33fe94d4-3ca4-48ee-8842-6c1166ee515b/200x200-logo-manpower.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_2802IK01OORA70QUFIPQ192H31-33fe94d4-3ca4-48ee-8842-6c1166ee515b-mbU.Nf2",
+    opportunities: 5,
+  },
+  // {
+  //   id: 4,
+  //   company: "Alicorp",
+  //   image: "https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg",
+  //   opportunities: 3,
+  // },
+  // {
+  //   id: 5,
+  //   company: "Gloria",
+  //   image: "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg",
+  //   opportunities: 6,
+  // },
+  // {
+  //   id: 6,
+  //   company: "Southern Copper",
+  //   image: "https://images.pexels.com/photos/3182773/pexels-photo-3182773.jpeg",
+  //   opportunities: 2,
+  // },
+  // {
+  //   id: 7,
+  //   company: "BBVA",
+  //   image: "https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg",
+  //   opportunities: 8,
+  // },
+  // {
+  //   id: 8,
+  //   company: "Interbank",
+  //   image: "https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg",
+  //   opportunities: 4,
+  // },
+  // {
+  //   id: 9,
+  //   company: "Banco de Crédito del Perú",
+  //   image: "https://images.pexels.com/photos/3184635/pexels-photo-3184635.jpeg",
+  //   opportunities: 6,
+  // },
+  // {
+  //   id: 10,
+  //   company: "LATAM Airlines",
+  //   image: "https://images.pexels.com/photos/912050/pexels-photo-912050.jpeg",
+  //   opportunities: 5,
+  // },
+  // {
+  //   id: 11,
+  //   company: "Cineplanet",
+  //   image: "https://images.pexels.com/photos/7991370/pexels-photo-7991370.jpeg",
+  //   opportunities: 3,
+  // },
+  // {
+  //   id: 12,
+  //   company: "Sodimac",
+  //   image: "https://images.pexels.com/photos/4483772/pexels-photo-4483772.jpeg",
+  //   opportunities: 7,
+  // },
+  // {
+  //   id: 13,
+  //   company: "Ripley",
+  //   image: "https://images.pexels.com/photos/3965545/pexels-photo-3965545.jpeg",
+  //   opportunities: 5,
+  // },
+  // {
+  //   id: 14,
+  //   company: "Falabella",
+  //   image: "https://images.pexels.com/photos/3184419/pexels-photo-3184419.jpeg",
+  //   opportunities: 9,
+  // },
+  // {
+  //   id: 15,
+  //   company: "InRetail",
+  //   image: "https://images.pexels.com/photos/5632398/pexels-photo-5632398.jpeg",
+  //   opportunities: 4,
+  // },
+  // {
+  //   id: 3,
+  //   company: "Telefónica",
+  //   image: "https://images.pexels.com/photos/3184454/pexels-photo-3184454.jpeg",
+  //   opportunities: 5,
+  // },
+  // {
+  //   id: 4,
+  //   company: "Alicorp",
+  //   image: "https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg",
+  //   opportunities: 3,
+  // },
+  // {
+  //   id: 5,
+  //   company: "Gloria",
+  //   image: "https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg",
+  //   opportunities: 6,
+  // },
+  // {
+  //   id: 6,
+  //   company: "Southern Copper",
+  //   image: "https://images.pexels.com/photos/3182773/pexels-photo-3182773.jpeg",
+  //   opportunities: 2,
+  // },
+  // {
+  //   id: 7,
+  //   company: "BBVA",
+  //   image: "https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg",
+  //   opportunities: 8,
+  // },
+  // {
+  //   id: 8,
+  //   company: "Interbank",
+  //   image: "https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg",
+  //   opportunities: 4,
+  // },
+  // {
+  //   id: 9,
+  //   company: "Banco de Crédito del Perú",
+  //   image: "https://images.pexels.com/photos/3184635/pexels-photo-3184635.jpeg",
+  //   opportunities: 6,
+  //},
+];
+
+export const events = [
+  {
+    id: 1,
+    time: "13:00",
+    title: "Cómo destacar en procesos de selección en empresas líderes",
+    company: "Ferreyros",
+    logo: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg",
+    speaker: "Ana María Torres",
+    role: "Senior Recruiter",
+    link: "#",
+  },
+  {
+    id: 2,
+    time: "14:30",
+    title: "Innovación y sostenibilidad en la minería",
+    company: "Antamina",
+    logo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg",
+    speaker: "Luis Gutiérrez",
+    role: "Gerente de Innovación",
+    link: "#",
+  },
+  {
+    id: 3,
+    time: "15:00",
+    title: "Transformación digital en la banca peruana",
+    company: "BBVA",
+    logo: "https://images.pexels.com/photos/210241/pexels-photo-210241.jpeg",
+    speaker: "María López",
+    role: "Chief Digital Officer",
+    link: "#",
+  },
+  {
+    id: 4,
+    time: "15:45",
+    title: "El futuro del e-commerce en Latinoamérica",
+    company: "Mercado Libre",
+    logo: "https://images.pexels.com/photos/756744/pexels-photo-756744.jpeg",
+    speaker: "Carlos Fernández",
+    role: "E-commerce Manager",
+    link: "#",
+  },
+  {
+    id: 5,
+    time: "16:30",
+    title: "Tecnología y telecomunicaciones en la nueva era",
+    company: "Claro",
+    logo: "https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg",
+    speaker: "Javier Ramírez",
+    role: "Director de Tecnología",
+    link: "#",
+  },
+  {
+    id: 6,
+    time: "17:15",
+    title: "Energías renovables y oportunidades en Perú",
+    company: "Enel",
+    logo: "https://images.pexels.com/photos/885350/pexels-photo-885350.jpeg",
+    speaker: "Claudia Medina",
+    role: "Especialista en Energías Renovables",
+    link: "#",
+  },
+  {
+    id: 7,
+    time: "18:00",
+    title: "Cómo construir una marca personal sólida",
+    company: "LinkedIn",
+    logo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+    speaker: "Fernando Ruiz",
+    role: "Community Manager",
+    link: "#",
+  },
+];
+
+export const recommendations = [
+  {
+    title: "Crea tu marca personal",
+    description:
+      "Muestra tus proyectos en un portafolio digital y mantén tu perfil de LinkedIn actualizado.",
+  },
+  {
+    title: "Refuerza tus habilidades blandas",
+    description:
+      "Comunicación, liderazgo y trabajo en equipo son tan importantes como lo técnico.",
+  },
+  {
+    title: "Aprende y certifícate",
+    description:
+      "Capacítate en nuevas tendencias y busca certificaciones que respalden tus competencias.",
+  },
+];
